959f443c07f82f64ae4ee088ebe528a1f72e624e
Chang Yao Chung <yaochung9744@gmail.com>
2021-11-17 02:22:47 +0000

    implement extract

--- lib/Trainer.cpp
+++ lib/Trainer.cpp
@@ -17,4 +17,40 @@ void Trainer::train(std::vector< std::pair<Map, MoveDirection> > steps){
 }
 
 torch::Tensor Trainer::extract(const Map map){
+    // Extract 9x9x5 from given map (#, ., $, %)
+    union{
+        Decimal input3D[4][9][9];
+        Decimal input1D[324];
+    } data;
+    int rows = map.size(), cols = map[0].size();
+    // Get man position
+    int manRow = -1, manCol = -1;
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            if(map[i][j] == '@'){
+                manRow = i, manCol = j;
+            }
+        }
+    }
+    for(int i=-4; i<=4; i++){
+        for(int j=-4; j<=4; j++){
+            int nextRow = manRow + i, nextCol = manCol + j;
+            if(nextRow < 0 || nextRow >= rows || nextCol < 0 
+            || nextCol >= cols || map[nextRow][nextCol] == '#'){
+                data.input3D[0][i+4][j+4] = 1.0;
+            }else{
+                switch(map[nextRow][nextCol]){
+                    case '.':
+                        data.input3D[1][i+4][j+4] = 1.0;
+                        break;
+                    case '$':
+                        data.input3D[2][i+4][j+4] = 1.0;
+                        break;
+                    case '%':
+                        data.input3D[3][i+4][j+4] = 1.0;
+                        break;
+                }
+            }
+        }
+    }
 }
