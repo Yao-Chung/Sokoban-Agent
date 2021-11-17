a54d834b9ec41f6b43b95fa6b6dd8517b1fe9385
LuisHsu <luishsuplus@gmail.com>
2021-11-17 01:36:24 +0000

    Define Trainer class

--- include/Trainer.hpp
+++ include/Trainer.hpp
@@ -4,12 +4,16 @@
 #include <utility>
 #include <vector>
 #include <defines.hpp>
+#include <torch/torch.h>
 
 class Trainer{
+public:
     Trainer(/*TODO: weights*/);
     ~Trainer();
     std::vector<Decimal> suggest(const Map map);
     void train(std::vector< std::pair<Map, MoveDirection> > steps);
+private:
+    torch::Tensor extract(const Map map);
 };
 
 #endif
---
+++ lib/Trainer.cpp
@@ -0,0 +1,20 @@
+#include <Trainer.hpp>
+
+Trainer::Trainer(/*TODO: weights*/){
+
+}
+
+Trainer::~Trainer(){
+
+}
+
+std::vector<Decimal> Trainer::suggest(const Map map){
+
+}
+
+void Trainer::train(std::vector< std::pair<Map, MoveDirection> > steps){
+
+}
+
+torch::Tensor Trainer::extract(const Map map){
+}
