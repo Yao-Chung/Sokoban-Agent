b4fd51dcf1930e77b8db35c7a633afc5bbbb9fbe
LuisHsu <luishsuplus@gmail.com>
2021-11-23 01:33:34 +0000

    Stream mode

--- exec/trainer.cpp
+++ exec/trainer.cpp
@@ -6,19 +6,19 @@
 int main(int argc, char const *argv[])
 {
     // Parse command-line arguments
-    if(argc != 3){
-        std::cerr << "Usage: " << argv[0] << " <solution_file> <model_file>" << std::endl;
+    if(argc < 3){
+        std::cerr << "Usage: " << argv[0] << " <solution_file> <model_file> [--stream]" << std::endl;
         return -1;
     }
-    // Read solution
-    std::vector< std::pair<Map, std::vector<MoveDirection>> > solutions = read_solutions(argv[1]);
-    // Create Trainer
     Trainer trainer(argv[2]);
-    // Give suggestion
-    std::vector<Decimal> suggest = trainer.suggest(solutions[0].first);
-    // Train model
-    trainer.train(solutions[0].first, solutions[0].second);
-    // Save model
+    if((argc > 3) && (std::string(argv[3]) == "--stream")){
+        // Stream mode
+        // TODO:
+    }else{
+        // Single file mode
+        std::vector< std::pair<Map, std::vector<MoveDirection>> > solutions = read_solutions(argv[1]);
+        trainer.train(solutions[0].first, solutions[0].second);
+    }
     trainer.save();
     return 0;
 }
